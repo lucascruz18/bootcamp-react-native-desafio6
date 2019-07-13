646e5ea628534d285fd1c08ff1298bc923a77249
Lucas <lucas@alternativainternet.com>
2019-07-13 11:35:59 -0300

    primeiro commit

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+export default function Repository() {
+  return (
+    <View>
+      <Text>Teste</Text>
+    </View>
+  );
+}
+
+Repository.navigationOptions = () => ({
+  title: 'Repository',
+});

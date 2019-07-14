dde79a9e1062f0476cd7f5ef0dea9e398223b1b9
Lucas <lucas@alternativainternet.com>
2019-07-14 13:58:54 -0300

    adicionado web view dos repositorios

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
-// import { Container } from './styles';
-export default function Repository() {
-  return (
-    <View>
-      <Text>Teste</Text>
-    </View>
-  );
+import { Web } from './styles';
+
+export default function Repository({ navigation }) {
+  const repository = navigation.getParam('repository');
+  return <Web source={{ uri: repository.html_url }} />;
 }
 
-Repository.navigationOptions = () => ({
-  title: 'Repository',
+Repository.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
+
+Repository.navigationOptions = ({ navigation }) => ({
+  title: navigation.getParam('repository').name,
 });
